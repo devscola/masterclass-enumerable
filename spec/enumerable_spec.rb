a083e0c67b8172c03bbc5a943cde945245926b8a
Eduardo Sebastian <esebastian@altiora.es>
2014-06-06 09:22:58 +0200

    Write the spec for the 'each' method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,6 +1,15 @@
 require './my_array'
 
+my_array = MyArray.new([1,2,3,4,5])
+
 describe "each" do
+  it "iterates over the entire collection" do
+    ruby_array = []
+    my_array.each do |i|
+      ruby_array << i
+    end
+    expect(ruby_array).to eq(my_array.to_a)
+  end
 end
 
 describe "select" do
