f3140beb956912ff435e926abdafc9c6e024aefb
Eduardo Sebastian <esebastian@altiora.es>
2014-06-06 09:27:52 +0200

    Write the implementation for the 'each' method

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,5 +1,10 @@
 module MyEnumerable
+  # enumerable is a private method in the class where the module is included
+
   def each
+    for element in enumerable
+      yield element
+    end
   end
 
   def select
